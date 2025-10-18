daae0c246b2a8205a5e918c8a3c752b08cde3424
rishukumark19 <rishukumark.spam@gmail.com>
2025-10-18 11:23:38 +0000

    Restore correct code

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 class Post(models.Model): 
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     body = models.TextField()
 
     def __str__(self):
