da327f5cf41c5d0eff9ef0217b0726316151dac5
rishukumark19 <rishukumark.spam@gmail.com>
2025-10-18 10:41:46 +0000

    Fix models/tests/views; convert Post.author to FK; remove formatting issues

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 from django.conf import settings
 
 
-class Post(models.Model):
+class Post(models.Model): 
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
     body = models.TextField()
 
     def __str__(self):
@@ -13,14 +13,4 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
-# Create your models here.
-from django.db import models
-from django.urls import reverse # new
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    body = models.TextField()
-    def __str__(self):
-        return self.title
-def get_absolute_url(self): # new
-    return reverse("post_detail", kwargs={"pk": self.pk})
+
